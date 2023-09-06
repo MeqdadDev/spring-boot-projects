ce536da796a5226ca8a4325bc1dddf47e6e2cfcd
MeqdadDev <meqdad.darweesh@gmail.com>
2023-09-06 21:48:31 +0300

    implementing custom user details service

---
+++ PokemonAPI/src/main/java/com/example/api/security/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.example.api.security;
+
+import com.example.api.models.UserEntity;
+import com.example.api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Username not found."));
+        return new User(user.getUsername(), user.getPassword(), user.getRoles());
+    }
+}
