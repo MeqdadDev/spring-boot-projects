31a6a9c98cc470810092d19343cedc338855884d
MeqdadDev <meqdad.darweesh@gmail.com>
2023-09-14 14:28:54 +0300

    Pokemon API - add JWT generator feature

--- PokemonAPI/src/main/java/com/example/api/security/SecurityConfig.java
+++ PokemonAPI/src/main/java/com/example/api/security/SecurityConfig.java
@@ -3,40 +3,49 @@ package com.example.api.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private JwtAuthEntryPoint authEntryPoint;
+
     private CustomUserDetailsService userDetailsService;
 
     @Autowired
-    public SecurityConfig(CustomUserDetailsService userDetailsService) {
+    public SecurityConfig(CustomUserDetailsService userDetailsService,
+                          JwtAuthEntryPoint authEntryPoint) {
         this.userDetailsService = userDetailsService;
+        this.authEntryPoint = authEntryPoint;
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
         http
                 .csrf(csrf -> csrf.disable())
-                .authorizeRequests()
-                .requestMatchers("/api/v1/auth/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .httpBasic(Customizer.withDefaults());
+                .authorizeHttpRequests(request -> request.requestMatchers("/api/v1/auth/**")
+                        .permitAll().anyRequest().authenticated())
+                .sessionManagement(manager -> manager.sessionCreationPolicy(STATELESS))
+                .exceptionHandling()
+                .authenticationEntryPoint(authEntryPoint);
+//                .and()
+//                .sessionManagement()
+//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and()
+//                .requestMatchers("/api/v1/auth/**").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .httpBasic(Customizer.withDefaults());
         return http.build();
 
 //        SecurityFilterChain in Spring Security 6.0+
---
+++ PokemonAPI/src/main/java/com/example/api/security/SecurityConstants.java
@@ -0,0 +1,6 @@
+package com.example.api.security;
+
+public class SecurityConstants {
+    public static final long JWT_EXPIRATION = 70000;
+    public static final String JWT_SECRET = "supposed_to_be_secret";
+}
