a8662befb6fc89dece373a69be1d7a05f81f9095
MeqdadDev <meqdad.darweesh@gmail.com>
2023-09-22 23:42:53 +0300

    Jwt - work on jwt request parts: header, signKey

---
+++ JwtAuthentication/src/main/java/com/springprojects/security/config/JwtService.java
@@ -0,0 +1,33 @@
+package com.springprojects.security.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+    private static final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+    public String extractUsername(String token) {
+        return null;
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+}
