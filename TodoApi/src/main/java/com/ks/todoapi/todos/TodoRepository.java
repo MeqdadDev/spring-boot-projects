a6e279296f85add250b32df24fbf7b55eb3d82bd
MeqdadDev <meqdad.darweesh@gmail.com>
2024-02-14 08:10:02 +0200

    TodoAPI: upload repository class for mongodb driver

---
+++ TodoApi/src/main/java/com/ks/todoapi/todos/TodoRepository.java
@@ -0,0 +1,10 @@
+package com.ks.todoapi.todos;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+@Repository
+public interface TodoRepository extends MongoRepository<Todo, String> {
+}
