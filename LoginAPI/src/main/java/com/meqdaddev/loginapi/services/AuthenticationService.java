c87766b7e999f8e74bcf3807b1b5338192608484
MeqdadDev <meqdad.darweesh@gmail.com>
2024-03-05 22:43:32 +0200

    LoginAPI: add auth service in services

---
+++ LoginAPI/src/main/java/com/meqdaddev/loginapi/services/AuthenticationService.java
@@ -0,0 +1,38 @@
+package com.meqdaddev.loginapi.services;
+
+import com.meqdaddev.loginapi.models.ApplicationUser;
+import com.meqdaddev.loginapi.models.Role;
+import com.meqdaddev.loginapi.repository.RoleRepository;
+import com.meqdaddev.loginapi.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Transactional
+public class AuthenticationService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public ApplicationUser registerUser(String username, String password) {
+        String encodedPassword = passwordEncoder.encode(password);
+        Role userRole = roleRepository.findByAuthority("USER").get();
+
+        Set<Role> authorities = new HashSet<>();
+
+        authorities.add(userRole);
+
+        return userRepository.save(new ApplicationUser(0, username, encodedPassword, authorities));
+    }
+}
