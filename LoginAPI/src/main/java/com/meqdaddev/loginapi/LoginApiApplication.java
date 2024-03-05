3029b4a4e3e609162c834221b8095ed7caa2103d
MeqdadDev <meqdad.darweesh@gmail.com>
2024-03-05 22:40:22 +0200

    LoginAPI: loading Roles and Admin User

--- LoginAPI/src/main/java/com/meqdaddev/loginapi/LoginApiApplication.java
+++ LoginAPI/src/main/java/com/meqdaddev/loginapi/LoginApiApplication.java
@@ -1,13 +1,39 @@
 package com.meqdaddev.loginapi;
 
+import com.meqdaddev.loginapi.models.ApplicationUser;
+import com.meqdaddev.loginapi.models.Role;
+import com.meqdaddev.loginapi.repository.RoleRepository;
+import com.meqdaddev.loginapi.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @SpringBootApplication
 public class LoginApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LoginApiApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner run(RoleRepository roleRepository, UserRepository userRepository, PasswordEncoder passwordEncoder){
+        return args -> {
+            if (roleRepository.findByAuthority("ADMIN").isPresent()) return;
+            Role adminRole = roleRepository.save(new Role("ADMIN"));
+            Role userRole = roleRepository.save(new Role("USER"));
+
+            Set<Role> roles = new HashSet<>();
+
+            roles.add(adminRole);
+
+            ApplicationUser admin = new ApplicationUser(1, "admin", passwordEncoder.encode("password"), roles);
+            userRepository.save(admin);
+        };
     }
 
 }
