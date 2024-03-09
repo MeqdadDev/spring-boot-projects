222edfaba7025456677c588d44b62af901b0f24b
MeqdadDev <meqdad.darweesh@gmail.com>
2024-03-09 11:21:28 +0200

    LoginAPI: add KeyGenerator utility

--- LoginAPI/src/main/java/com/meqdaddev/loginapi/utils/KeyGeneratorUtility.java
+++ LoginAPI/src/main/java/com/meqdaddev/loginapi/utils/KeyGeneratorUtility.java
@@ -7,7 +7,7 @@ public class KeyGeneratorUtility {
     public static KeyPair generateRsaKey() {
 
         KeyPair keyPair;
-
+        // Exception Handling
         try {
             KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
             keyPairGenerator.initialize(2048);
