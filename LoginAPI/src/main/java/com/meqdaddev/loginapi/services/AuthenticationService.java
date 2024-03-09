624ccc7076b20709daee05cfa88a97f9b9907be9
MeqdadDev <meqdad.darweesh@gmail.com>
2024-03-09 17:02:41 +0200

    LoginAPI: Creating the loginUser method in AuthenticationService

--- LoginAPI/src/main/java/com/meqdaddev/loginapi/services/AuthenticationService.java
+++ LoginAPI/src/main/java/com/meqdaddev/loginapi/services/AuthenticationService.java
@@ -1,10 +1,15 @@
 package com.meqdaddev.loginapi.services;
 
+import com.meqdaddev.loginapi.dto.LoginResponseDTO;
 import com.meqdaddev.loginapi.models.ApplicationUser;
 import com.meqdaddev.loginapi.models.Role;
 import com.meqdaddev.loginapi.repository.RoleRepository;
 import com.meqdaddev.loginapi.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,6 +30,12 @@ public class AuthenticationService {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private TokenService tokenService;
+
     public ApplicationUser registerUser(String username, String password) {
         String encodedPassword = passwordEncoder.encode(password);
         Role userRole = roleRepository.findByAuthority("USER").get();
@@ -34,5 +45,20 @@ public class AuthenticationService {
         authorities.add(userRole);
 
         return userRepository.save(new ApplicationUser(0, username, encodedPassword, authorities));
+    }
+
+    public LoginResponseDTO loginUser(String username, String password) {
+        try {
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(username, password)
+            );
+
+            String token = tokenService.generateJwt(authentication);
+
+            return new LoginResponseDTO(userRepository.findByUsername(username).get(), token);
+
+        } catch (AuthenticationException e) {
+            return new LoginResponseDTO(null, "");
+        }
     }
 }
